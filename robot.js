9e1968db3115c9f8b6d2ed514db23fd1704489d9
Meagan Blais <meaganblais@gmail.com>
2017-11-11 09:21:19 -0500

    adding colour variables

--- robot.js
+++ robot.js
@@ -1,3 +1,20 @@
+// From a design perspective, the syntax for jQuery is: When I do this, make the CSS do this.
+// The more common slogan is: Find something, do something. 
+// -Chris Coyler
+
+// When eyes button is clicked, toggle laser class
 $('.flash').click(function() {
   $('.brain').toggleClass('laser');
+});
+
+// a function should only do one thing. 1. something useful or 2. return a value
+
+// When colour button is clicked, pick a number 0-255
+$('.colour').click(function() {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  
+  //Display the number in an alert window
+  alert(red + "," + green + "," + blue);
 });
